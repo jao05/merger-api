debbcf8c350c4c0baafc33c52a65f8aac4579957
Jesse Outlaw <JesseOutlaw03@gmail.com>
2018-11-25 13:18:14 -0500

    Eliminated unecessary whitespace

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require('express');
- const app = express();
+const app = express();
 
- const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
- app.get('/api/*', (req, res) => {
-   res.json({ok: true});
- });
+app.get('/api/*', (req, res) => {
+  res.json({ok: true});
+});
 
- app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
 
- module.exports = {app};
+module.exports = {app};
