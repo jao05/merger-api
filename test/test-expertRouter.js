d8c7e16b2a0332cf3461baaf5b63d703e704486b
Jesse Outlaw <JesseOutlaw03@gmail.com>
2018-12-03 17:52:42 -0500

    Complete tests for GET requests to expertRouter

--- test/test-expertRouter.js
+++ test/test-expertRouter.js
@@ -133,6 +133,66 @@ describe('Serving expert assets', function {
 
 
   // Test the GET request for the '/expert' endpoint
+  describe('the GET endpoint', function {
+
+  	it('should return experts that meet specific criteria', function() {
+
+  		// strategy:
+        //    1. get back all experts returned by GET request to `/userCompany`
+        //    2. prove the res has the right status & data type
+        //    3. prove the number of experts we got back is equal to number
+        //       in db.
+        //
+        // We need to have access to mutate and access `res` across
+        // `.then()` calls below, so declare it here so we can modify in place
+        let res;
+
+        return chai.request(app)
+          .get('/expert?type=Legal')          
+          .then(function(_res) {
+            // so subsequent .then blocks can access response object
+            res = _res;
+            expect(res).to.have.status(200);                      
+            expect(res.body.expertCompanies).to.have.lengthOf.at.least(1);  // otherwise our db seeding didn't work
+          })  
+  	});
+
+  	it('should return experts with the correct fields', function() {
+  		
+  		// Strategy: Get back all experts, and ensure they have expected keys
+
+        let resExpert;
+        return chai.request(app)
+          .get('/expert?type=Legal')
+          .then(function(res) {
+            expect(res).to.have.status(200);
+            expect(res).to.be.json;            
+            expect(res.body.expertCompanies).to.be.a('array');
+            expect(res.body.expertCompanies).to.have.lengthOf.at.least(1);
+
+            res.body.expertCompanies.forEach(function(expertCompany) {
+              expect(expertCompany).to.be.a('object');
+              expect(expertCompany).to.include.keys(
+                'type',
+                'name',
+                'contact', 
+                'location'
+              );
+            }); 
+            
+            resExpert = res.body.expertCompanies[0];
+            return Negotiator.findById(resExpert.id);           
+           })
+          .then(function(expertComp) {
+            
+            expect(resExpert.type).to.equal(expertComp.type);
+            expect(resExpert.name).to.equal(expertComp.name);
+            expect(resExpert.contact).to.equal(expertComp.contact);            
+            expect(resExpert.location).to.equal(expertComp.location);           
+          });
+    });	
+  });
+
   
 
 
